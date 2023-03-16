fa12fa5119c057f058df36224eb99653a60f953f
Ali <jiwanix@gmail.com>
2023-03-16 13:34:11 -0400

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Write me response as a kind and empathetic therapist. Feel free to ask questions or suggest strategies depending on the prompt.
+
+Response:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}/n`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,59 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const onGenerateClick = () => {
+    callGenerateEndpoint();
+  };
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    // const response = await fetch('/api/generate', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ userInput }),
+    // });
+    
+    // const data = await response.json();
+    // console.log(data);
+    
+    console.log("Calling OpenAI...");
+    try {
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userInput }),
+      });
+
+      const data = await response.json();
+      const { output } = data;
+      console.log("OpenAI replied...", output.text);
+
+      setApiOutput(output.text);
+    } catch (error) {
+      console.error("Error calling OpenAI API:", error);
+      setApiOutput("Error generating text, please try again.");
+    }
+
+    setIsGenerating(false);
+  };
+
+  const onUserChangedText = (event) => {
+    //console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,13 +62,51 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Talk to your therapist, Socrates</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask it anything you want!</h2>
           </div>
         </div>
+
+        <textarea
+          className="prompt-box"
+          placeholder="start typing here"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
+       
+
+        <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+
+
+
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
+      {/* <div className="prompt-container">
+        <textarea placeholder="start typing here" className="prompt-box" />
+      </div> */}
+
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
